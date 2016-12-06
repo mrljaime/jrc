898c2b26666a39903fdaa0b76fbd8b880a75cb41
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2016-12-06 15:39:38 -0600

    JRC

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,61 @@
 /**
  * Created by josejaime on 06/12/16.
  */
+/** This shit is used to stored new users at database */
+var LocalStrategy = require("passport-local").Strategy;
+var User = require("../models/user");
+
+/** To the universe */
+module.exports = function(passport) {
+    /** Serialize user */
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    /** Deserialize user */
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    /** Local sign up */
+    passport.use("local-signup", new LocalStrategy(
+        {
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+        function(req, username, password, done) {
+            console.log("JAIME >>>>> Before Async");
+            /** Async */
+            process.nextTick(function() {
+
+                /**
+                 *  1. Find user by email
+                 */
+                User.findOne({'username': username}, function(err, user) {
+                    console.log("JAIME >>>>> Inside searching");
+                    if (err) {
+                        return done(err);
+                    }
+
+                    if (user) {
+                        return done(null, false);
+                    }
+
+                    var newUser = new User();
+                    newUser.email = email;
+                    newUser.password = newUser.generateHash(password);
+
+                    newUser.save(function(err) {
+                        if (err) {
+                            throw err;
+                        }
+                        return done(null, newUser);
+                    });
+                });
+            });
+        }
+    ));
+};
--- models/user.js
+++ models/user.js
@@ -1,3 +1,25 @@
 /**
  * Created by josejaime on 06/12/16.
  */
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
+
+/** This is just a web page with single user, the user schema just need username, email and password  */
+var userSchema = mongoose.Schema({
+    email: String,
+    username: String,
+    password: String
+});
+
+/** I don't want to stored my password without hashing before */
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+/** Some other responsablity of this schema es validate the password */
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+/** Creating the model */
+module.exports = mongoose("User", userSchema);

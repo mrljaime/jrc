c0802bb4457cd19a74b6a8f8563721480f59434d
José Jaime Ramírez Calvo <mr.ljaime@gmail.com>
2017-09-24 19:15:37 -0500

    Cambios, y replace para markup en frontend

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -20,7 +20,12 @@ application.factory("httpInterceptor", ["$q", "$location", function($q, $locatio
     }
 }]);
 
-application.config(function($stateProvider, $httpProvider, $urlRouterProvider) {
+application.config(function($stateProvider, $httpProvider, $urlRouterProvider, $sceProvider) {
+
+    /**
+     * To compile html
+     */
+    $sceProvider.enabled(false);
 
     /**
      * Set default router
@@ -57,6 +62,32 @@ application.config(function($stateProvider, $httpProvider, $urlRouterProvider) {
 
 
     $httpProvider.interceptors.push("httpInterceptor");
+});
+
+application.filter("passionMarkup", function() {
+
+    return function(text) {
+        /**
+         * Strong with * [.*?] *
+         */
+        text = text.replace(/\*(.*?)\*/g, "<strong>$1</strong>");
+
+        /**
+         * Href
+         */
+        text = text.replace(/\<(.*?)\|(.*?)\>/g, "<a href='$1' target='_blank'>$2</a>");
+
+        /**
+         * Italic
+         */
+
+        text = text.replace(/\_\_(.*?)\_\_/g, function(match, g1) {
+            return "<i>" + g1 + "</i>";
+        });
+
+
+        return text;
+    }
 });
 
 application.run(function($rootScope, $window) {
